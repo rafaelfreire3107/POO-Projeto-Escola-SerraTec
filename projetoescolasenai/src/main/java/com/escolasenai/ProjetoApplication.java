e37076837cf1a52167f87d6d20308889429b4a6f
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-14 09:51:53 -0300

    Update ProjetoApplication.java

--- projetoescolasenai/src/main/java/com/escolasenai/ProjetoApplication.java
+++ projetoescolasenai/src/main/java/com/escolasenai/ProjetoApplication.java
@@ -2,43 +2,52 @@ package com.escolasenai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.List;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ProjetoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoApplication.class, args);
-		Professor professor = new Professor("Arthur", "12345678910", "Ciências");
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoApplication.class, args);
 
-		Disciplina portugues = new Disciplina("Portugues", professor);
-
-
-		Aluno aluno1 = new Aluno("Rafael Freire", "15582754963", "0001");
-		Aluno aluno2 = new Aluno("Luis Filipe", "07555384792", "0002");
-
-		portugues.adicionarAluno(aluno1);
-        portugues.adicionarAluno(aluno2);
-
-		
-		Nota nota1 = new Nota(portugues, 8.5);
-        Nota nota2 = new Nota(portugues, 9.5);
-
-		aluno1.adicionarNota(nota1);
-        aluno2.adicionarNota(nota2);
-
-		System.out.println("Disciplina: "+ portugues.getNome());
-        System.out.println("Professor: "+ portugues.getProfessor().getNome());
-
-		List<Aluno> alunos = portugues.getAlunos();
         
-		for (Aluno aluno : alunos) {
-            System.out.println("Aluno: " + aluno.getNome() + " - Matrícula: " + aluno.getMatricula());
-            
-			List<Nota> notas = aluno.getNota();
-            for (Nota nota : notas) {
-                System.out.println("Nota na disciplina " + nota.getDisciplina().getNome() + ": " + nota.getValor());
-            }
-	}
+        Aluno.alimentaAluno();
+        Disciplina.alimentaDisciplina();
+        Nota.alimentaNota();
 
-	}}
+        
+        menuPrincipal();
+    }
+
+    public static void menuPrincipal() {
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("Escolha como prosseguir: ");
+            System.out.println("1. Acessar o menu do aluno.");
+            System.out.println("2. Acessar o menu do professor.");
+            System.out.println("0. Sair.");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    Aluno.menuAluno();
+                    break;
+                case 2:
+                    Professor.menuProfessor();
+                    break;
+                case 0: // Correção da opção de sair
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Insira um dígito válido.");
+                    break;
+            }
+        } while (opcao != 0);
+
+        sc.close(); 
+    }
+}
